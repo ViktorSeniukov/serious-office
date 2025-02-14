0b0d952983932323c97caaa1b128f414a03c9ac0
Viktor Seniukov <spinnekopser@gmail.com>
2025-02-14 12:39:43 +0100

    Added qrcode router

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import TestView from '../views/TestView.vue';
 import SmokeView from '../views/SmokeView.vue';
 import NotFoundView from '../views/NotFoundView.vue';
+import { useQrCodeRedirect } from '@/features/useQrCodeRedirect.ts';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +16,11 @@ const router = createRouter({
       path: '/smoke',
       name: 'smoke',
       component: SmokeView
+    },
+    {
+      path: '/qrcode',
+      name: 'qrcode',
+      redirect: () => useQrCodeRedirect()
     },
     {
       path: '/:pathMatch(.*)*',
