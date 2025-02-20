2a3d51c940f7783aff3f321d1cdf16b04500af3a
Viktor Seniukov <spinnekopser@gmail.com>
2025-02-20 12:38:29 +0100

    try fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/serious-office/',
+  base: '/',
   plugins: [
     vue(),
     vueDevTools()
