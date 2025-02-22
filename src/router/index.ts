317480d21eec77be75c1826b3be8193e2626d42b
Viktor Seniukov <spinnekopser@gmail.com>
2025-02-22 18:55:18 +0100

    added poo view

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import TestView from '../views/TestView.vue';
 import SmokeView from '../views/SmokeView.vue';
 import NotFoundView from '../views/NotFoundView.vue';
 import QrCodeView from '../views/QrCodeView.vue';
+import TookAPoopView from '@/views/TookAPoopView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,6 +17,11 @@ const router = createRouter({
       path: '/smoke',
       name: 'smoke',
       component: SmokeView
+    },
+    {
+      path:'/took-a-poop',
+      name: 'took_a_poop',
+      component: TookAPoopView
     },
     {
       path: '/qrcode',
