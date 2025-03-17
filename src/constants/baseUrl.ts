7cc9a6971b08ae7dd3296baa8dd190b990817620
Viktor Seniukov <spinnekopser@gmail.com>
2025-03-17 22:01:45 +0100

    try transfer to / fetch path

--- src/constants/baseUrl.ts
+++ src/constants/baseUrl.ts
@@ -1,5 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL as string || '';
 const API_VERSION = import.meta.env.VITE_API_VERSION as string || '';
 
-export const BASE_API_URL = `${API_URL}/api/${API_VERSION}`;
+export const BASE_API_URL = `/api/${API_VERSION}`;
 // export const BASE_API_URL = 'http://localhost:3000/api/v1';
